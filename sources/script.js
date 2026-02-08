d5af8c8eaea82bdbb3017e9dd022af2a273e57d3
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-08 17:12:29 -0300

    solving step two requirements
    using Math library method random and using the method toFixed of type number

--- sources/script.js
+++ sources/script.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+// this function will return a number between 0 and 2
+function getRandomNumber(){
+   return Math.random(3).toFixed(0); //making sure that will be a integer number
+} 
+/* this function will call getRandomNumber to obtain a number and based on
+will return wich elemnt computer choice between the three possible 
+rock,paper,scissors for this time. 
+*/ 
+function getComputerChoice(){
+  let randomNumber = getRandomNumber();
+  if(randomNumber == 0){
+    return "rock"
+  }else if(randomNumber == 1){
+    return "paper"
+  }else{
+    return "scissors"
+  }
+ }
+let number = getRandomNumber();//making sure this function will return a number between 0 and 2
+console.log(number);
+let choice = getComputerChoice();
+console.log(choice);
